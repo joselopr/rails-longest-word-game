8a3a797db6275a01332b87d30219ec1e0aa4b8fa
jose prieto <josel0priet0@gmail.com>
2021-05-24 16:48:02 +0200

    words game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = []
@@ -5,5 +7,22 @@ class GamesController < ApplicationController
   end
 
   def score
+    word_letters = params[world].upcase.split('')
+    word.letters.each do |word_letter|
+      if word_letters.count(word_letter) > @letters.count(word_letter)
+        @score = "sorry but #{word_letters.uppercase} cant be built out of #{letters.join(', ')}";
+      elsif dictionary?(@word) == false;
+        @score = "sorry but #{word_letters.uppercase} dont seems to be a valid Enlgish word..";
+      else
+        @score = "Congratulation! #{word_letters.uppercase} is a valid word!"
+      end
+    end
+  end
+  def dictionary?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    serialized_words = File.read(url)
+
+    words = json.parse(serialized_words)
+    words['found']
   end
 end
